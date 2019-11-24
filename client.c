21900d3f23a91791cfebea119e5e82c87c0c4054
katolikov <55280831+katolikov@users.noreply.github.com>
2019-11-24 22:08:32 +0300

    New version

--- client.c
+++ client.c
@@ -14,39 +14,51 @@ int main(){
   FILE *shell;
   struct sockaddr_in addr;
 
-  printf("\n\033[1;35m[INFO] Client working: \n");
+  printf("\n\033[1;35m[INFO]\033[0m Client working:\033[0m \n");
   printf("\033[0m");
-  sock = socket(AF_INET, SOCK_STREAM, 0);
+  sock = socket(AF_INET, SOCK_STREAM, 0);//создаем гнездо
+  //– SOCK_STREAM — виртуальный канал с предварительно установленным соединением;
+  //– SOCK_DGRAM — межпроцессное взаимодействие с помощью дейтаграмм;
+  //– SOCK_SEQPACKET — виртуальный канал с предварительно установленным соединением с возможностью передачи сообщений максимальной длины.
+
   if (sock < 0)
   {
-    perror("\033[1;31m[ERROR] Socket failed\n");
+    perror("\033[1;31m[ERROR]\033[0m Socket failed\n");
     printf("\033[0m");
     return 1;
   }
 
-  printf("\n\033[1;32m[DEBUG] Sock fd = %d\n\n", sock);
+  printf("\n\033[1;32m[DEBUG]\033[0m Sock fd = %d\n\n", sock);
+  // для соединения нужен адрес интерфейса внутренней петли (loopback interface).
+  // В программе ему соответствует константа INADDR_LOOPBACK.
+  // Пакеты, направляемые по этому адресу, в сеть не попадают. Вместо этого они передаются стеку протоколов TCP/IP как только что принятые.
+  // Таким образом моделируется наличие виртуальной сети, в которой можно отлаживать сетевые приложения.
   addr.sin_family = AF_INET;
   addr.sin_port = htons(6666);
-  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);//(127.0.0.1) всегда приписывается локальному хосту через закольцовывающий интерфейс
 
-  if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+  // Системный вызов connect вызывается в клиентском процессе для установления соединения с серверным гнездом.
+  // Если sid обозначает потоковое гнездо, то между клиентским (КГ) и серверными (СГ) гнездами устанавливается
+  // соединение с использованием виртуального канала. Потоковое гнездо клиента может соединяться с гнездом сервера только один раз.
+  if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0)//подключаемся по имени к виртуальному адрессному пространству
   {
-    perror("\033[1;31m[ERROR] Bind failed");
+    perror("\033[1;31m[ERROR]\033[0m Connect failed");
     printf("\033[0m");
     return 1;
   }
-  listen(sock, 0);
+
   shell = popen("cal", "r");
   if(shell < 0)
   {
-    perror("\033[1;31m[ERROR] Popen failed\n");
+    perror("\033[1;31m[ERROR]\033[0m Popen failed\n");
     printf("\033[0m");
     return 1;
   }
   fread(buf, 1, sizeof(buf), shell);
   pclose(shell);
-  printf("\033[1;35m[INFO] Send to server:\n\n---------------------\n%s---------------------\n", buf);
-  send(sock, buf, strlen(buf), 0);
+  printf("\033[1;35m[INFO]\033[0m Send to server:\n\n---------------------\n\033[1;35m%s\033[0m---------------------\n", buf);
+  send(sock, buf, strlen(buf), 0); //0 - обычные сообщения в гнездо sock из buf длинны buf
+  
   close(sock);
   printf("\033[0m");
   return 0;
--- server.c
+++ server.c
@@ -11,29 +11,29 @@
 int day_num(int n){
   int day = n % 7;
   switch (day){
-     case 0:
-        printf("\033[1;35mSaturday\n\n");
-        break;
-     case 1:
-        printf("\033[1;35mSunday\n\n");
-        break;
      case 2:
-        printf("\033[1;35mMonday\n\n");
+        printf("\033[1;35mSaturday\033[0m\n\n");
         break;
      case 3:
-        printf("\033[1;35mTuesday\n\n");
+        printf("\033[1;35mSunday\033[0m\n\n");
         break;
      case 4:
-        printf("\033[1;35mWednesday\n\n");
+        printf("\033[1;35mMonday\033[0m\n\n");
         break;
      case 5:
-        printf("\033[1;35mThursday\n\n");
+        printf("\033[1;35mTuesday\033[0m\n\n");
         break;
      case 6:
-        printf("\033[1;35mFriday\n\n");
+        printf("\033[1;35mWednesday\033[0m\n\n");
+        break;
+     case 0:
+        printf("\033[1;35mThursday\033[0m\n\n");
+        break;
+     case 1:
+        printf("\033[1;35mFriday\033[0m\n\n");
         break;
      default:
-        perror("\033[1;31m[ERROR] Incorrect data\n");
+        perror("\033[1;31m[ERROR]\033[0m Incorrect data\n");
         printf("\033[0m");
         return 1;
   }
@@ -46,37 +46,47 @@ int main()
     char buf[BUF_SIZE];
     char buf1[BUF_SIZE];
     int bytes_read;
-    printf("\n\033[1;35m[INFO] Server working: \n");
+    printf("\n\n\t\t\033[1;5;31mS\033[1;32mO\033[1;33mC\033[1;34mK\033[1;35mE\033[1;36mT\033[1;37mS\033[0m\n");
+    printf("\n\033[1;35m[INFO]\033[0m Server working:\033[0m \n");
     printf("\033[0m");
     sock1 = socket(AF_INET, SOCK_STREAM, 0);
     if(sock1 < 0)
     {
-        perror("\033[1;31m[ERROR] Socket failed\n");
-        printf("\033[0m");
-        return 1;
-    }
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(6666);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if(bind(sock1, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
-        perror("\033[1;31m[ERROR] Bind\n");
+        perror("\033[1;31m[ERROR]\033[0m Socket failed\n");
         printf("\033[0m");
         return 1;
     }
 
-    listen(sock1, 0);
-    sock = accept(sock1, 0, 0);
+    //номер порта который намерен занять процесс. Если значение этого поля равно нулю, то операционная система сама выделит свободный номер порта для сокета
+    addr.sin_family = AF_INET;// заполняем поле семейства адресов
+    addr.sin_port = htons(6666);//преобразует узловой порядок расположения байтов положительного короткого целого hostshort в сетевой порядок расположения байтов.
+    //соединение с клиентами через любой интерфейс, задаем в качестве адреса константы INADDR_ANY
+    addr.sin_addr.s_addr = htonl(INADDR_ANY); // локальный IP адрес, преобразует узловой порядок расположения байтов положительного целого hostlong в сетевой порядок расположения байтов.
+    //(0.0.0.0)
+    if(bind(sock1, (struct sockaddr *)&addr, sizeof(addr)) < 0) //Он связывает сокет со всеми доступными интерфейсами addr - имя гнезда
+    {
+        perror("\033[1;31m[ERROR]\033[0m Bind\n");
+        printf("\033[0m");
+        return 1;
+    }
+    listen(sock1,0); //0 - неограниченное число запросов
+
+    // Системный вызов accept вызывается в серверном процессе для установления соединения с клиентским гнездом, которое осуществило системный вызов connect (можно ассоциировать с открытием файла на чтение/запись).
+    // Если addr или len имеет значение NULL, этот системный вызов не передает имя клиентского гнезда обратно в вызывающий процесс.
+    sock = accept(sock1, 0, 0); //этот системный вызов не передает имя клиентского гнезда обратно в вызывающий процесс
 
     if(sock < 0)
     {
-            perror("\033[1;31m[ERROR] Accept\n");
+            perror("\033[1;31m[ERROR]\033[0m Accept\n");
             printf("\033[0m");
             return 1;
     }
 
     while(1){
-        bytes_read = recv(sock, buf, BUF_SIZE - 75, 0);
+      // Этот системный вызов принимает сообщение через гнездо, указанное в sid_fd. Принятое сообщение записывается в buffer, а максимальный размер buffer задается в sizeof(buffer).
+      // Системный вызов recv возвращает число байтов, записанных в buffer, в случае успеха, и -1 — неудачи.
+      // 4 Аргумент flag равен 0 для обычных сообщений, MSG_OOB — для высокоприоритетных.
+        bytes_read = recv(sock, buf,BUF_SIZE, 0); //прием сообщения из удаленного гнезда
 
         if(bytes_read <= 0)
         {
@@ -89,22 +99,22 @@ int main()
 
         if(shell < 0)
         {
-          perror("\033[1;31m[ERROR] Popen failed\n");
+          perror("\033[1;31m[ERROR]\033[0m Popen failed\n");
           printf("\033[0m");
           return 1;
         }
 
         fscanf(shell, "%d", &day);
         pclose(shell);
-        printf("\n\033[1;35mDay num: %d ",day);
+        printf("\n\033[1;35m[ANSWER]\033[0m Day num: \033[1;35m%d\033[0m ",day);
         day_num(day);
         close(sock);
         close(fd);
         system("rm uniguie");
         printf("\033[0m");
     }
-    else printf("Rename your file: uniguie\n");
+    else printf("\n\033[1;31m[ERROR]\033[0m Rename your file: uniguie\n");
     }
-
+    close(sock1);
     return 0;
 }
